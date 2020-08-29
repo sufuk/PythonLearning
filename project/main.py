7f8825805bb5447592d8c7cdc284abf787c3af1b
Semih Ufuk Güler <sufukguler@gmail.com>
2020-08-29 15:07:21 +0300

    solved every tme qr problem

--- project/main.py
+++ project/main.py
@@ -3,13 +3,16 @@ import time
 import smtplib
 import telegram
 import os
+from selenium.webdriver.chrome.options import Options
 
 my_token = ''
 
 def send(msg, chat_id, token=my_token):
 	bot = telegram.Bot(token=token)
 	bot.sendMessage(chat_id=chat_id, text=msg)
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome("chromedriver.exe", options=chrome_options)
 driver.get("https://web.whatsapp.com/")
 a = input("Please switch the user u wanna check")
 if a == "1":
