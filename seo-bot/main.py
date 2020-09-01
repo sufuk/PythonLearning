388d394dfea6cf731201a51fd559c43da43c9f68
Semih Ufuk Güler <sufukguler@gmail.com>
2020-09-01 21:57:09 +0300

    kok

--- seo-bot/main.py
+++ seo-bot/main.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
-import time
-import smtplib
-import telegram
-import os
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+profile = webdriver.FirefoxProfile()
+profile.set_preference('network.proxy.type', 1)
+profile.set_preference('network.proxy.socks', '127.0.0.1')
+profile.set_preference('network.proxy.socks_port', 9150)
+binary = FirefoxBinary(r"C:\Tor Browser\Browser\firefox.exe")
+driver = webdriver.Firefox(profile, binary)
+driver.get("http://stackoverflow.com")
+driver.save_screenshot("screenshot.png")
+driver.quit()
