b47f5e8a953ab4e4514a2b53c381dd26572a3e6b
Semih Ufuk Güler <sufukguler@gmail.com>
2020-09-28 15:36:08 +0300

    just opened cam

--- opencvprojects/CIGDEM/main.py
+++ opencvprojects/CIGDEM/main.py
@@ -1,3 +1,15 @@
 import cv2
 import imutils
 import numpy
+
+camera = cv2.VideoCapture(0)
+while(True):
+    ret, frame = camera.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
